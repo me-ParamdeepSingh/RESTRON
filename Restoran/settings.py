de97f7374fde9516fe2f1c58e575847ab706ff48
Paramdeep Singh <=>
2024-02-21 14:56:06 +0530

    setting1.7

--- Restoran/settings.py
+++ Restoran/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-i)nbm!si!$u_n0a5m)y=gs$k0*l8jafddy3fx3a*r0_^g0l@(f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -141,7 +141,7 @@ STATICFILES_DIRS=[
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static'),
 MEDIA_URL = 'media/',
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 
